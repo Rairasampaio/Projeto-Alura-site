ceb93df7f59bfc9d3469ceb86062ef24c8efa690
Rairasampaio <rairasampaio1@gmail.com>
2024-09-07 17:14:25 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -28,8 +28,153 @@ let dados = [
       descricao: "Zombicide: Night of the Living Dead é um jogo baseado no filme de George A. Romero de 1968, onde os jogadores assumem o papel de sobreviventes enfrentando carniçais. Com regras simples, o jogo oferece dois modos: o Modo Romero, em preto e branco, que permite alternar identidades dos personagens, e o Modo Zombicide, com ação intensa contra os mortos-vivos. O jogo traz as mecânicas clássicas de Zombicide, miniaturas detalhadas e 10 cenas exclusivas do filme. O destino dos sobreviventes está em suas mãos. Conseguirão sobreviver à noite?",
       link: "https://pt.wikipedia.org/wiki/Zombicide",
     
-}
+},
+{     Título: "Everdell",
+      descricao: "Em Everdell, os jogadores constroem uma cidade em uma floresta mágica. Ao longo das quatro estações, os jogadores coletam recursos, constroem edifícios e atraem animais para sua cidade.",
+      link: "https://en.wikipedia.org/wiki/Everdell"
+},
+{
+      Título: "Clank!",
+      descricao: "Em Clank!, os jogadores exploram as profundezas de um vulcão em busca de tesouros. A cada turno, eles rolam dados para mover-se e realizar ações, mas cuidado para não fazer muito barulho e despertar o dragão!",
+      link: "https://en.wikipedia.org/wiki/Clank"
+
+},
+{
+      Título: "Scythe",
+      descricao: "Scythe é um jogo de eurogame ambientado em um mundo pós-guerra. Os jogadores assumem o controle de facções em um continente em reconstrução, coletando recursos, desenvolvendo sua tecnologia e expandindo seu território. O jogo oferece um equilíbrio entre estratégia e engine building.",
+      link:"https://en.wikipedia.org/wiki/Scythe"
+},
+{     Título: "Wingspan",
+      descricao: "Wingspan é um jogo temático sobre a observação de aves. Os jogadores coletam aves, constroem seus habitats e buscam completar objetivos científicos. O jogo combina mecânicas de engine building com um tema relaxante e visualmente deslumbrante.",
+      link:"https://en.wikipedia.org/wiki/Wingspan_(board_game)"
+},
+{
+      Título: "Carcassonne",
+      descrição: "Em Carcassonne, os jogadores constroem uma paisagem medieval, colocando peças de terreno e colocando seus seguidores para pontuar. Cidades, estradas, mosteiros e campos oferecem diferentes oportunidades de pontuação, mas a estratégia é fundamental para garantir a vitória.",
+      link: "https://pt.wikipedia.org/wiki/Carcassonne"
+},
+{
+      titulo: "Orgulho e Preconceito",
+      descricao: "Romance de Jane Austen que explora temas como amor, classe social e casamento, através da história de Elizabeth Bennet e Fitzwilliam Darcy. ",
+      link: "https://pt.wikipedia.org/wiki/Orgulho_e_Preconceito"
+},
+{
+      titulo: "Moby Dick",
+      descricao: "Romance de aventura de Herman Melville que narra a obsessão do capitão Ahab por caçar a baleia branca Moby Dick. ",
+      link: "https://pt.wikipedia.org/wiki/Moby_Dick"
+},
+{     titulo: "Os Miseráveis",
+      descricao: "Epopeia histórica de Victor Hugo que acompanha a vida de Jean Valjean, um ex-condenado em busca de redenção, em meio à França do século XIX. ",
+      link:"https://pt.wikipedia.org/wiki/Os_Miser%C3%A1veis"
+},
+{
+      titulo: "Crime e Castigo",
+      descricao: "Romance psicológico de Fyodor Dostoiévski que acompanha as angústias de Raskolnikov, um estudante que comete um assassinato e lida com as consequências morais de seu ato. ",
+      link: "https://pt.wikipedia.org/wiki/Crime_e_Castigo"
+},
+{
+      titulo: "O Pequeno Príncipe",
+      descricao: "Fábula poética de Antoine de Saint-Exupéry que narra a história de um piloto que encontra um pequeno príncipe em um deserto e embarca em uma jornada de autoconhecimento. ",
+      link: "https://pt.wikipedia.org/wiki/O_Pequeno_Príncipe"
+},
+{
+      titulo: "Cem Anos de Solidão",
+      descricao: "Romance épico de Gabriel García Márquez que narra a história de sete gerações da família Buendía em Macondo, uma cidade fictícia na Colômbia. ",
+      link:"https://pt.wikipedia.org/wiki/Cem_Anos_de_Solid%C3%A3o"
+},
+{
+      titulo: "O Senhor dos Anéis",
+      descricao: "Trilogia épica de fantasia de J.R.R. Tolkien que narra a jornada de Frodo Bolseiro para destruir o Um Anel. ",
+      link: "https://pt.wikipedia.org/wiki/O_Senhor_dos_Anéis"
+},
+{
+      titulo: "1984",
+      descricao: "Distopia de George Orwell que retrata um futuro totalitário onde a liberdade individual é suprimida e a vigilância é constante. ",
+      link: "https://pt.wikipedia.org/wiki/1984"
+},
+{
+      titulo: "O Nome da Rosa",
+      descricao: "Romance histórico de Umberto Eco que mistura investigação policial com questões filosóficas e teológicas, ambientado em um mosteiro medieval. ",
+      link: "https://pt.wikipedia.org/wiki/O_Nome_da_Rosa"
+},
+{
+      titulo: "O Hobbit",
+      descricao: "Aventura épica de J.R.R. Tolkien que narra a jornada de Bilbo Bolseiro em busca de um tesouro e um anel mágico. ",
+      link: "https://pt.wikipedia.org/wiki/O_Hobbit"
+},
+{
+      titulo: "A Metamorfose",
+      descricao: "Conto surrealista de Franz Kafka que narra a história de Gregor Samsa, um homem que acorda transformado em um inseto e enfrenta a solidão e a rejeição. ",
+      link: "https://pt.wikipedia.org/wiki/A_Metamorfose"
+},
+{
+      titulo: "O Processo",
+      descricao: "Romance existencial de Franz Kafka que narra a história de Josef K., um homem acusado de um crime indefinido e submetido a um processo absurdo. ",
+      link: "https://pt.wikipedia.org/wiki/O_Processo"
+},
+{
+      titulo: "Ulysses",
+      descricao: "Romance experimental de James Joyce que narra um dia na vida de Leopold Bloom em Dublin, utilizando uma linguagem inovadora e complexa. ",
+      link: "https://pt.wikipedia.org/wiki/Ulysses"
+},
+{
+      titulo: "Em Busca do Tempo Perdido",
+      descricao: "Obra monumental de Marcel Proust que explora a memória, a consciência e o amor através da vida do narrador. ",
+      link: "https://pt.wikipedia.org/wiki/Em_Busca_do_Tempo_Perdido"
+},
+{
+      titulo: "O Estrangeiro",
+      descricao: "Romance existencialista de Albert Camus que narra a história de Meursault, um homem indiferente que é condenado à morte por um crime aparentemente sem motivo. ",
+      link: "https://pt.wikipedia.org/wiki/O_Estrangeiro"
+},
+{
+      titulo: "A Divina Comédia",
+      descricao: "Poema épico de Dante Alighieri que narra a jornada de Dante pelos três reinos da vida após a morte: Inferno, Purgatório e Paraíso. ",
+      link: "https://pt.wikipedia.org/wiki/Divina_Comédia"
+},
+{
+      titulo: "Memórias Póstumas de Brás Cubas",
+      descricao: "Romance de Machado de Assis que narra a vida de Brás Cubas, um defunto autor que relata sua vida de forma irônica e crítica. ",
+      link: "https://pt.wikipedia.org/wiki/Mem%C3%B3rias_P%C3%B3stumas_de_Br%C3%A1s_Cubas"
+},
+{
+      titulo: "O Morro dos Ventos Uivantes",
+      descricao: "Romance gótico de Emily Brontë que narra a paixão tumultuada entre Catherine Earnshaw e Heathcliff. ",
+      link: "https://pt.wikipedia.org/wiki/O_Morro_dos_Ventos_Uivantes"
+},
+{
+      titulo: "As Crônicas de Nárnia",
+      descricao: "Série de fantasia de C.S. Lewis que narra as aventuras de crianças em um mundo mágico, Nárnia.",
+      link: "https://pt.wikipedia.org/wiki/As_Crônicas_de_Nárnia"
+},
+{
+      titulo: "A Guerra dos Tronos",
+      descricao: "Primeiro volume da saga épica de George R.R. Martin, ambientada em um mundo medieval fantástico com intrigas políticas, batalhas épicas e magia.",
+      link: "https://pt.wikipedia.org/wiki/A_Guerra_dos_Tronos"
+},
+{
+      titulo: "O Nome do Vento",
+      descricao: "Primeiro volume da trilogia Crônica do Matador do Rei, de Patrick Rothfuss, que narra a história de Kvothe, um jovem talentoso e misterioso.",
+      link: "https://pt.wikipedia.org/wiki/O_Nome_do_Vento"
+},
+{
+      titulo: "Duna",
+      descricao: "Clássico da ficção científica de Frank Herbert, ambientado em um planeta desértico e repleto de intrigas políticas e religiosas.",
+      link: "https://pt.wikipedia.org/wiki/Duna"
+},
+{
+      titulo: "O Príncipe Cruel",
+      descricao: "Primeiro volume da série Corte de Espinhos e Rosas, de Sarah J. Maas, que narra a história de Feyre, uma humana que é levada para o reino dos Fae.",
+      link: "https://pt.wikipedia.org/wiki/O_Príncipe_Cruel"
+},
+{
+      titulo: "O Feiticeiro de Terramar",
+      descricao: "Saga de fantasia de Ursula K. Le Guin que narra as aventuras de Ged, um jovem mago em um mundo mágico e complexo.",
+      link: "https://pt.wikipedia.org/wiki/O_Feiticeiro_de_Terramar"
+},
+{
+      titulo: "A Biblioteca dos Mortos",
+      descricao: "Série de fantasia urbana de Michael Scott que narra as aventuras de Ash, um jovem que descobre que sua família é guardiã de uma biblioteca mágica.",
+      link: "https://en.wikipedia.org/wiki/The_Library_of_the Dead"
+}     
 ];
-
-
-
