d2fed85c2772676c5fd188baf28fada899c3b97b
Rairasampaio <rairasampaio1@gmail.com>
2024-09-06 21:27:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
 function pesquisar() {
     // Obtém a seção onde os resultados da pesquisa serão exibidos
     let section = document.getElementById("resultados-pesquisa");
-    console.log(section); // Loga a seção no console para verificação
 
+    console.log(section); // Loga a seção no console para verificação
     let campoPesquisa = document.getElementById("campo-pesquisa").value
     console.log(campoPesquisa)
 
     //se campoPesquisa for uma string sem nada
     if (campoPesquisa == "") {
-      section.innerHTML = "Não localizado"
+      section.innerHTML = "Nenhum Resultado"
           return
     }
-  
     campoPesquisa = campoPesquisa.toLowerCase()
 
     // Inicializa uma string vazia para armazenar os resultados da pesquisa
     let resultados = "";
     let titulo = "";
     let descricao = "";
-    let tags = "";
 
     // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
       titulo = dado.titulo.toLowerCase()
       descricao = dado.descricao.toLowerCase()
-      tags = dado.tags.toLowerCase
+      
 
       //se titulo includes campoPesquisa
       if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa))
-      if (tags.includes(campoPesquisa))
-        {
+        
+      {
         // Cria um novo elemento
       resultados += `
       <div class="item-resultado">
@@ -41,16 +39,15 @@ function pesquisar() {
       </div>
     `;
       }
+
     }
-    
+
     if(!resultados){
-      section.innerHTML = <p>"Não Localizado. Você precisa pesquisar algo"</p>
+      section.innerHTML = "Não Localizado. Você precisa pesquisar algo"
       return
     }
 
     // Atribui o HTML gerado à seção de resultados
     section.innerHTML = resultados;
   }
-
 // console.log(dados);
-
